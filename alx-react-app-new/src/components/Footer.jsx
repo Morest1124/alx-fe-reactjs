2cf7461649cdb6511378c95e6f4b36ad9e0d0b76
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-02 15:06:10 +0200

    Initialize React app with Vite and ESLint setup and styling components
    Add initial project files for a React application using Vite, including basic components, configuration files, and ESLint setup. Includes .gitignore, README, and starter source files for a minimal working app. Learning how to use inline stying

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,5 @@
+<footer>
+  <p style={{ color: "green", fontSize: "20px" }}>© 2023 City Lovers</p>
+</footer>;
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+<main>
+  <p style={{ color: "blue", fontFamily: "jetBrain" }}>
+    I love to visit New York, Paris, and Tokyo.
+  </p>
+</main>;
+
+export default MainContent;
