4e5ade69165f3c98f894587b9be9b794ca0ed528
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-04 00:48:10 +0200

    Add a router

--- my-company/my-company/src/App.jsx
+++ my-company/my-company/src/App.jsx
@@ -7,22 +7,12 @@ import Services from "./components/Services";
 
 function App() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="<About/>">About</a>
-        </li>
-        <li>
-          <a href="<Services/>">Services</a>
-        </li>
-        <li>
-          <a href="<Contact/>">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <home />
+      <About />
+      <Services />
+      <Contact />
+    </>
   );
 }
 
---
+++ my-company/my-company/src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+<nav>
+  <ul>
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="<About/>">About</a>
+    </li>
+    <li>
+      <a href="<Services/>">Services</a>
+    </li>
+    <li>
+      <a href="<Contact/>">Contact</a>
+    </li>
+  </ul>
+</nav>;
