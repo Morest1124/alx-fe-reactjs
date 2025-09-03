a18d8305fe90bf9c5f36b4b396673a24e7c8677a
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-03 09:27:37 +0200

    Corrected the syntax

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,13 +1,25 @@
-import ProfilePage from "./ProfilePage";
-import UserContext from "./UserContext";
+import WelcomeMessage from "./components/WelcomeMessage";
+import { useState } from "react";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
-  return 
-  <UserContext.Provider = {userData}>
-    <ProfilePage userData={userData} />;
-    </UserContext.Provider>
+  return (
+    <>
+      <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile
+        name="Morest"
+        age="19"
+        bio="We, the unwilling, led by the unknowing, are doing the impossible for the ungrateful. We have done so much, for so long, with so little, we are now qualified to do anything with nothing."
+      />
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
