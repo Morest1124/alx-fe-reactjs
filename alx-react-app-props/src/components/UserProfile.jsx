dd7cbb44366e51db7409ac3e2e5dc1c1c1bac5f5
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-03 10:19:12 +0200

    corrected user profile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,12 +1,9 @@
 import { createContext, useContext } from "react";
-const UserProfile = (props) => {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  );
-};
+// 1. Create the Context
+const UserContext = createContext(null);
 
+// 2. Define the components
+const UserProfile = () => {
+  const { name, bio } = useContext(UserContext);
+};
 export default UserProfile;
