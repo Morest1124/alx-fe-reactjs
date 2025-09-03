0a8b740a01ea92c656862c57a4bba50caffac20d
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-03 09:48:20 +0200

    add usercontex

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -4,10 +4,15 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import UserConext from "./UserContext";
 
 function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
   return (
     <>
+      <UserConext.Provider ProfilePage={userData}>
+        <ProfilePage userData={userData} />;
+      </UserConext.Provider>
       <WelcomeMessage />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserProfile
