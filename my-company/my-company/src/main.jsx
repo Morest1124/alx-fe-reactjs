dc1727e8edd345f9fca46dfb6497f4785a05c2b4
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-04 00:40:28 +0200

    Add a router

--- my-company/my-company/src/main.jsx
+++ my-company/my-company/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/Home", element: <Home /> },
+  { path: "/About", element: <About /> },
+  { path: "/Services", element: <Services /> },
+  { path: "/Contact", element: <Contact /> },
+  { path: "*", element: <NotFoundPage /> },
+]);
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <RouterProvider router={router} />
+
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
