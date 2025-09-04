d7ba5ff6ae915b2882115672e7f99cb055b966a7
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-04 12:41:57 +0200

    Add the corrected upload

---
+++ my-company20/my-company/src/App.jsx
@@ -0,0 +1,46 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link style={{ backgroundColor: "red" }} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link style={{ backgroundColor: "red" }} to="/about">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link style={{ backgroundColor: "red" }} to="/services">
+              Services
+            </Link>
+          </li>
+          <li>
+            <Link style={{ backgroundColor: "red" }} to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
