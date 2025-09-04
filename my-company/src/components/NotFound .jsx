e363ed6e6285d2e341fb1d7e5b18fef797727120
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-04 12:59:37 +0200

    Added the correct folder

---
+++ my-company/src/components/NotFound .jsx
@@ -0,0 +1,12 @@
+function NotFound() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>About Us</h1>
+      <p>
+        <link href="./Home.jsx">Nothing here</link>
+      </p>
+    </div>
+  );
+}
+
+export default NotFound;
