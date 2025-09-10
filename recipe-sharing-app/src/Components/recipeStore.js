e2358ac603931c02713c3c203dd7c53792d46e03
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-10 11:24:03 +0200

    place components in one file recipeStore

--- recipe-sharing-app/src/Components/recipeStore.js
+++ recipe-sharing-app/src/Components/recipeStore.js
@@ -1,3 +1,20 @@
+// RecipeList component
+import { useRecipeStore } from "./recipeStore";
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 // AddRecipeForm component
 import { useState } from "react";
 import { useRecipeStore } from "./recipeStore";
