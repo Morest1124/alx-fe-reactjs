5c981ffa8a13e26a4fa6533d8aa30fab415a0697
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-11 22:54:39 +0200

    update App.jsx Imported links from route

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,6 +4,7 @@ import AddRecipeForm from "./components/AddRecipeForm";
 import addRecipe from "./components/addRecipe";
 import updateRecipe from "./components/updateRecipe";
 import deleteRecipe from "./components/deleteRecipe";
+import EditRecipeForm from "./components/EditRecipeForm";
 import { BrowserRouter, Route, Routes, router } from "react-router-dom";
 import "./App.css";
 
@@ -15,6 +16,8 @@ function App() {
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipes/:id" element={<RecipeDetails />} />
         <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+        <Route path="/recipes/:id/" element={<FavoritesList />} />
+        <Route path="/recipes/:id/" element={<RecommendationsList />} />
       </Routes>
     </router>
   );
