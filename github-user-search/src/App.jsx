127af9b06ba696f84dca180805cf675e16afe619
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-15 21:36:59 +0200

    Added github user  search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Github User Search</h1>
+      <p>Welcome to the Github User Search App!</p>
+    </div>
+  );
+}
+
+export default App;
