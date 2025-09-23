69227e3b8e96f977f0a93a78339bfa3172134e3b
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-23 02:15:53 +0200

    Implemented responsive design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,15 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img
-        className="rounded-full w-36 h-36 mx-auto"
-        src="https://morest1124.github.io/Morest-Burawa-Portfolio/My_profile-removebg-preview.png"
-        alt="User"
-      />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 max-w-xsw">
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">
+        <img
+          className="rounded-full mx-auto w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36"
+          src="https://morest1124.github.io/Morest-Burawa-Portfolio/My_profile-removebg-preview.png"
+          alt="User profile"
+        />
+        John Doe
+      </h1>
+      <p className="text-gray-600 text-base sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
