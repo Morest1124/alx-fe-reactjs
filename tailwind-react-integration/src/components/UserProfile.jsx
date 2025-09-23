2811df93f00c7a66be0271112f666184bdce1d42
Morest1124 <morest1124burawa2005@gmail.com>
2025-09-23 02:19:39 +0200

    Add condition for medium devices

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 max-w-xsw">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 max-w-xs md:max-w-sm ">
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">
         <img
           className="rounded-full mx-auto w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36"
